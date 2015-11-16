a424d6213c6eee9db4687d4ea244f98f7f6b4db0
TimKuip <timkuiper02@hotmail.com>
2015-11-16 07:48:49 +0100

    Containerclass added

---
+++ Containing/Containing/Container.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "Container.h"
+
+
+Container::Container(string id, string type, string recordld, string aankomstDatum, double aankomstTijd, double uitlaadTijd, string aankomstVervoer, string aankomstBedrijf, int positieX, int positieY, int positieZ, string eigenaar, int containerNr, int vertrekDatum, double inlaadStart, double inlaadEind, int vertrekVervoer, int vertrekBedrijf, double lengte, double breedte, double hoogte, int leegGewicht, int inhoudGewicht, string inhoudNaam, string inhoudSoort, string gevaar, string ISO)
+{
+	Id = id;
+	Type = type;
+	Recordld = recordld;
+	AankomstDatum = aankomstDatum;
+	AankomstTijd = aankomstTijd;
+	UitlaadTijd = uitlaadTijd;
+	AankomstVervoer = aankomstVervoer;
+	AankomstBedrijf = aankomstBedrijf;
+	PositieX = positieX;
+	PositieY = positieY;
+	PositieZ = positieZ;
+	Eigenaar = eigenaar;
+	ContainerNr = containerNr;
+	VertrekDatum = vertrekDatum;
+	InlaadStart = inlaadStart;
+	InlaadEind = inlaadEind;
+	VertrekVervoer = vertrekVervoer;
+	VertrekBedrijf = vertrekBedrijf;
+	Lengte = lengte;
+	Breedte = breedte;
+	Hoogte = hoogte;
+	LeegGewicht = leegGewicht;
+	InhoudGewicht = inhoudGewicht;
+	InhoudNaam = inhoudNaam;
+	InhoudSoort = inhoudSoort;
+	Gevaar = gevaar;
+	ISO = ISO;
+
+
+
+
+}
+
+
+
+//Verder geen functies? Word puur verplaatst door voortuigen?
+
+Container::~Container()
+{
+	//Iemand werking van deconstructors? 
+}
