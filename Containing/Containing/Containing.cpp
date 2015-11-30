504e22e1e9e9b4632336433470a8b6ac0be1721d
alberthoogterp <albert_dino@live.nl>
2015-11-30 13:26:59 +0100

    sockets verwijderd

--- Containing/Containing/Containing.cpp
+++ Containing/Containing/Containing.cpp
@@ -2,33 +2,34 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
 using namespace std;
 
-
-int main()
+class controller
 {
-	Afstandberekenaar ab;
-	ab.KortsteRoute("kraan", "boot");
-
-	/*initSockets();
-	Client* client = new Client(50007);
-	string name = client->conversation("Name = ");
-	for (;;)
+	int main(int argc, _TCHAR* argv[])
 	{
-		string text = client->conversation(name + "> ");
-		if (text == "halt") break;
-	}
-	delete client;
-	exitSockets();
+		Afstandberekenaar ab;
+		cout << ab.KortsteRoute("kraan", "boot");
+		/*initSockets();
+		Client* client = new Client(50007);
+		string name = client->conversation("Name = ");
+		for (;;)
+		{
+			string text = client->conversation(name + "> ");
+			if (text == "halt") break;
+		}
+		delete client;
+		exitSockets();
 
-	cout << "Accepting clients ...\n";
-	initSockets();
-	Server server(50007);
-	for (;;)
-	{
-		server.acceptClient();
+		cout << "Accepting clients ...\n";
+		initSockets();
+		Server server(50007);
+		for (;;)
+		{
+			server.acceptClient();
+		}
+		exitSockets();
+		*/
 	}
-	exitSockets();
-	*/
-}
+};
